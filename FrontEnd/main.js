18e5c2c83dc826f3279540be7e6c8b04275ce913
yanis <yanismehalaine@outlook.fr>
2023-12-16 06:08:39 +0100

    feat(main.js): gallery is now imported from backend

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,6 +1,20 @@
 import { getWork } from "./get_data.js";
 
 const workData = await getWork();
-
+console.log(workData);
 //GET DOM
 const galleryElement = document.querySelector(".gallery");
+
+
+for (let i = 0; i < workData.length; i++) {
+    const figElement = document.createElement("figure");
+    const imgElement = document.createElement("img");
+    const captionElement = document.createElement("figcaption");
+    imgElement.src = workData[i].imageUrl;
+    imgElement.alt = workData[i].title;
+    captionElement.innerHTML = workData[i].title;
+
+    figElement.appendChild(imgElement);
+    figElement.appendChild(captionElement);
+    galleryElement.appendChild(figElement);
+}
