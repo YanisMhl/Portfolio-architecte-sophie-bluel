01a0a1dfa798322894d37b6215e65e066d864749
yanis <yanismehalaine@outlook.fr>
2023-12-17 03:36:19 +0100

    feat(login.js): user can connect to db with connexion form now

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,4 +1,14 @@
 import { loginUser } from "./get_data.js";
 
-const user = await loginUser("sophie.bluel@test.tld", "S0phie");
+// const user = await loginUser("sophie.bluel@test.tld", "S0phie");
+
+const formulaireConnexion = document.querySelector(".connexion-form");
+console.log(formulaireConnexion);
+formulaireConnexion.addEventListener("submit", async function(event) {
+    event.preventDefault();
+    const email = event.target.querySelector("[name=email]").value;
+    const password = event.target.querySelector("[name=password]").value;
+    const user = await loginUser(email, password);
+    console.log(user);
+});
 
