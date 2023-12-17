48c76809a2850e7dcbb49ec7ad88b7f85b307aa5
yanis <yanismehalaine@outlook.fr>
2023-12-17 10:22:32 +0100

    feat(user.js): added user class

---
+++ FrontEnd/assets/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(id, token) {
+        this.id = id;
+        this.token = token;
+    }
+
+    displayInfo() {
+        console.log(`id : ${this.id}\ntoken : ${this.token}`);
+    }
+}
