b09672acfbf050ea36caddd9f88160d9178f4a7f
yanis <yanismehalaine@outlook.fr>
2023-12-17 03:20:30 +0100

    feat(login.js/get_data.js): created loginUser function and retrieved user data in login.js

--- FrontEnd/get_data.js
+++ FrontEnd/get_data.js
@@ -1,4 +1,31 @@
 export async function getWork() {
-    const data = await fetch('http://localhost:5678/api/works').then(response => response.json());
+    const data = await fetch("http://localhost:5678/api/works").then(response => response.json());
     return data;
+}
+
+export async function loginUser(userEmail, userPassword) {
+    const userRaw = {
+        email: userEmail,
+        password: userPassword
+    };
+    const user = JSON.stringify(userRaw);
+
+    try {
+        const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: user
+        });
+        if (response.ok) {
+            console.log("Connexion réussie");
+            const data = await response.json();
+            return data;
+        } else {
+            //404 : utilisateur n'existe pas
+            //401 : mauvais mot de passe
+            throw new Error(`${response.status} : ${response.statusText}`);
+        }
+    } catch(err) {
+        console.log(err);
+    }
 }
---
+++ FrontEnd/login.js
@@ -0,0 +1,4 @@
+import { loginUser } from "./get_data.js";
+
+const user = await loginUser("sophie.bluel@test.tld", "S0phie");
+
